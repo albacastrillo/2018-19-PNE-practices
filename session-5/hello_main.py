3592770e91c296f5b67d7ad6a409b865a0e93160
albacastrillo <albacperote@gmail.com>
2019-02-06 18:59:55 +0100

    Just trying to import function hello from file Hello

---
+++ session-5/hello_main.py
@@ -0,0 +1,9 @@
+from Hello import hello
+
+# Name of the file without extension
+
+print("Hello from the main program")
+hello()
+print("bye bye")
+
+# Rightclick on the folder: Mark directory as --> Sources Root. Then the icon of the folder will be blue
