5570fcfa76720f1f0156a63dbdf92f21132721c5
albacastrillo <albacperote@gmail.com>
2019-02-19 16:35:38 +0100

    client session 7

--- session-7/Client.py
+++ session-7/Client.py
@@ -1,13 +1,11 @@
 # Programming our first client
-
 import socket
 
 # Create a socket  for communicating with the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 print("Socket created")
 
-PORT = 8080
+PORT = 8081
 IP = "212.128.253.64"
 
 s.connect((IP, PORT))
@@ -23,3 +21,4 @@ print(msg)
 s.close()
 
 print("The end")
+
