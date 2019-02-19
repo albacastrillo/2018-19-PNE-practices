11694b88da566cb4b617168837b4ef851f6d3270
albacastrillo <albacperote@gmail.com>
2019-02-19 16:36:14 +0100

    seq main practice 1

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -3,15 +3,16 @@ from Seq import Seq
 seq1 = Seq("ATTCGATCC")
 seq2 = Seq("AAAGG")
 seq3 = seq1.complement()
-seq4 = seq3.reverse().strbase
+seq4 = seq3.reverse()
 
 seq_list = [seq1, seq2, seq3, seq4]
 counter = 1
+bases = ['A', 'C', 'T', 'G']
 
 for seq in seq_list:
     print("Sequence", counter, ": {}".format(seq))
     print("     Length: {}".format(seq.len()))
-    bases = ['A', 'C', 'T', 'G']
+
     for base in bases:
         print("     Bases count: {}".format(seq.count(base)))
         print("     Bases percentage: {}".format(seq.perc(base)))
