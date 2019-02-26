8d35ece4bab2d7f9c311ec1ff437ac28bef3b47c
albacastrillo <albacperote@gmail.com>
2019-02-26 16:57:26 +0100

    Client from practice 2 extra introduced into a while loop

--- P2-extra/Client-P2EXTRA.py
+++ P2-extra/Client-P2EXTRA.py
@@ -1,26 +1,26 @@
 import socket
 
-# Create a socket  for communicating with the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket created")
+while True:
+    # Create a socket  for communicating with the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# SERVER IP, PORT
-PORT = 8089
-IP = "212.128.253.108"
+    # SERVER IP, PORT
+    PORT = 8089
+    IP = "212.128.253.108"
 
-# The client is not blocking the server anymore
-sequence = input("Introduce a sequence: ")
+    # The client is not blocking the server anymore
+    sequence = input("Introduce a sequence: ")
 
-# Establish the connection to the Server (IP, PORT)
-s.connect((IP, PORT))
+    # Establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
 
-# Send the request message to the server
-s.send(str.encode(sequence))
+    # Send the request message to the server
+    s.send(str.encode(sequence))
 
-# Receive the servers response
-msg = s.recv(2048).decode("utf-8")
+    # Receive the servers response
+    msg = s.recv(2048).decode("utf-8")
 
-# Print the server's response
-print("Message from the server: {}".format(msg))
+    # Print the server's response
+    print("Message from the server: {}".format(msg))
 
-s.close()
+    s.close()
