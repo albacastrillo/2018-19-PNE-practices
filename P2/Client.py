e41ad5d547e21227641d6807f18a7fdf68d1beac
albacastrillo <albacperote@gmail.com>
2019-02-26 16:14:31 +0100

    CLIENT FROM PRACTICE 2 DONE

--- P2/Client.py
+++ P2/Client.py
@@ -4,13 +4,18 @@ from Seq import Seq
 while True:
 
     # SERVER IP, PORT
-    IP = "212.128.253.88"
-    PORT = 8080
+    IP = "212.128.253.108"
+    PORT = 8089
 
     # The client is not blocking the server anymore
     sequence = input("Introduce a sequence: ")
-    msg = Seq(sequence).reverse().complement()
-    #message = msg.str
+    seq1 = Seq(sequence)
+    seq2 = Seq(seq1.complement())
+    seq3 = Seq(seq2.reverse())
+
+    bases = "A", "C", "G", "T"
+    sequences = seq1, seq2, seq3
+    num_seq = 0
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,12 +23,12 @@ while True:
     s.connect((IP, PORT))
 
     # Send the request message to the server
-    s.send(str.encode(msg))
+    s.send(str.encode(seq3.strbase))
 
     # Receive the servers response
-    response = s.recv(2048).decode()
+    response = s.recv(2048).decode("utf-8")
 
     # Print the server's response
-    print("The complement of the sequence is: {}".format(response))
+    print(response)
 
     s.close()
